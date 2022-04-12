99a83e253d2ad854014ed3a8ba34e517a8ba4bbc
Dx1ed <92229650+TheyCallMeDx1@users.noreply.github.com>
2022-04-12 14:00:51 -0600

    Create Scripts.lua

---
+++ Scripts.lua
@@ -0,0 +1,8 @@
+-- NamelessWare(Private):
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheyCallMeDx1/NamelessWare/main/Private.lua'))()
+
+-- NamelessWare.Aiming(Pre-Private):
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheyCallMeDx1/NamelessWare/main/Aiming.lua'))()
+
+-- AimingConfig(Aiming)
+-- https://raw.githubusercontent.com/TheyCallMeDx1/NamelessWare/main/AimingConfig.lua
