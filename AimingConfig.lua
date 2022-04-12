bfeb580cd2d415a6fa477e0c4236e38e8a6ad4e7
Dx1ed <92229650+TheyCallMeDx1@users.noreply.github.com>
2022-04-12 13:30:33 -0600

    Create AimingConfig.lua

---
+++ AimingConfig.lua
@@ -0,0 +1,5 @@
+-- Paste this next to the aiming script/loadstring and configure this however you like
+getgenv().SilentAimbotKey = 'b'; getgenv().AimlockKey = 'q';  _G.WEBHOOK_URL = '';
+
+-- If the above doesn't work try this
+SilentAimbotKey = 'b'; AimlockKey = 'q';  _G.WEBHOOK_URL = '';
